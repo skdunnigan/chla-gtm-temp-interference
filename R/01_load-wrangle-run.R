fa6f07f9a1f5b79011863ab88521233d4d512f42
Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2021-06-29 15:43:16 -0400

    updates in calculations for p constant

--- R/01_load-wrangle-run.R
+++ R/01_load-wrangle-run.R
@@ -14,33 +14,23 @@ dat <- readxl::read_xlsx(here::here('data', 'data.xlsx')) %>%
 
 Tr <- 20 # enter reference temperature
 
-# 02 create dilution plot ----------------------------------------------------
-
-## 02.1 create plotting function, call it 'lin_plot()' ----
-lin_plot <- function(x) {
+# 02 create initial dilution plot ----------------------------------------------------
 
 chla_title <- expression(paste("Chlorophyll ", italic("a "), mu*"g/L"))
 chla_RFU_title <- expression(paste("Chlorophyll ", italic("a "), mu*"g/L", " RFU"))
 
-  dat %>%
-    ggplot(mapping = aes(x = temp_c, y = x, color = dilution_percent)) +
-    geom_point(size = 3) +
-    stat_smooth(method = "lm", se = FALSE) +
-    # ggpubr::stat_regline_equation(label.y.npc = 'bottom', show.legend = FALSE, size = 5) +
-    scale_color_discrete(name = "Dilution Factor") +
-    # xlim(0, 40) +
-    theme_bw() +
-    theme(legend.position = "right",
-          axis.title = element_text(size = 12),
-          text = element_text(size = 12)) +
-    labs(y = chla_RFU_title,
-         x = 'Temperature ('~degree*C*')')
-
-}
-
-## 02.2 plot of lm Chla_rfu ~ Temperature ----
-a <- lin_plot(dat$chla_rfu)
-a
+plot.a <- dat %>%
+            ggplot(mapping = aes(x = temp_c, y = chla_rfu)) +
+            stat_smooth(method = "lm", aes(color = dilution_percent), se = FALSE) +
+            geom_point(size = 3) +
+            scale_color_discrete(name = "Dilution Factor") +
+            theme_bw() +
+            theme(legend.position = "right",
+                  axis.title = element_text(size = 12),
+                  text = element_text(size = 12)) +
+            labs(y = chla_RFU_title,
+                 x = 'Temperature ('~degree*C*')')
+plot.a
 
 # 03 assess for slope:intercept constant across all concentrations ---------------------------------------
 
@@ -76,64 +66,35 @@ ratios <- dplyr::left_join(m, b, by = "dilution_percent") %>%
 # remove unnecessary df
 rm(m, b, lm_out, diag)
 
+# derive p ----------------------------------------------------------------
 
-# 04 adjust for reference temp at 20C -------------------------------------
+plot.b <- ratios %>%
+            ggplot(aes(x = b, y = m)) +
+            geom_point(aes(color = dilution_percent), size = 3) +
+            stat_smooth(method = "lm", se = FALSE, color = "black") +
+            scale_color_discrete(name = "Dilution Factor") +
+            theme_bw() +
+            theme(legend.position = "right",
+                  axis.title = element_text(size = 12),
+                  text = element_text(size = 12)) +
+            labs(x = "Intercept",
+                 y = "Slope")
+plot.b
 
-dat2 <- dat %>%
-        dplyr::mutate(delta_T = temp_c - Tr)
+## calculate slope for slope ( = y) vs intercept ( = x) of the relationship
 
-chla_RFU_title <- expression(paste("Chlorophyll ", italic("a "), mu*"g/L", " RFU"))
+p.lm <- ratios %>% lm(m ~ b, data = .)
+p.lm.origin <- ratios %>% lm(m ~ 0 + b, data = .)
 
-dat2 %>%
-  ggplot(mapping = aes(x = delta_T, y = chla_rfu, color = dilution_percent)) +
-  geom_point(size = 3) +
-  stat_smooth(method = "lm", se = FALSE) +
-  scale_color_discrete(name = "Dilution Factor") +
-  theme_bw() +
-  theme(legend.position = "right",
-        axis.title = element_text(size = 12),
-        text = element_text(size = 12)) +
-  labs(y = chla_RFU_title,
-       x = expression(paste(Delta, 'Temperature ', "(", degree, "C)")),
-       caption = expression(paste('Reference temperature: ', 20, degree, "C")))
+p.lm.tidy <- broom::tidy(p.lm)
+
+p.lm.origin <- broom::tidy(p.lm.origin)
 
 
-# 05 calculate p ----------------------------------------------------------
+## get p
 
-lm_out <- dat2 %>%
-          dplyr::group_by(dilution_percent) %>%
-          do(broom::tidy(lm(chla_rfu ~ delta_T, data = .)))
-
-diag <- dat2 %>%
-        group_by(dilution_percent) %>%
-        do(broom::glance(lm(chla_rfu ~ delta_T, data = .)))
-
-##check out model outputs
-lm_out
-diag
-
-## pull out slope and intercept
-m <- lm_out %>%
-  dplyr::filter(term == 'delta_T') %>%
-  dplyr::rename(m = estimate) %>%
-  dplyr::select(-term)
-
-b <- lm_out %>%
-  dplyr::filter(term == '(Intercept)') %>%
-  dplyr::select(dilution_percent, estimate) %>%
-  dplyr::rename(b = estimate)
-
-## calculate p for each dilution
-p_all <- dplyr::left_join(m, b, by = "dilution_percent") %>%
-         dplyr::right_join(dat2, by = "dilution_percent") %>%
-         dplyr::mutate(p = m/b)
-
-# get average temperature coefficient for each concentration at adjusted temperature
-p <- mean(p_all$p, na.rm = TRUE)
-p_sd <- sd(p_all$p, na.rm = TRUE)
-
-p_exp <- paste0(round(p, digits = 3), "??", round(p_sd, digits = 3))
-
+p <- p.lm.tidy[[2,2]]
+p.origin <- p.lm.origin[[1,2]]
 
 
 # 06 apply adjustment to chla values --------------------------------------
@@ -144,23 +105,40 @@ Fr <- function(x, y){
   y / (1 + p*(x - Tr))
 }
 
-dat_corr <- dat2 %>%
-            mutate(chla_r = Fr(temp_c, chla_rfu))
+Fr_origin <- function(x, y){
+  y / (1 + p.origin*(x - Tr))
+}
+
+dat_corr <- dat %>%
+            mutate(chla_r = Fr(temp_c, chla_rfu),
+                   chla_r_origin = Fr_origin(temp_c, chla_rfu))
+
 
 # 07 create plot with corrected chlorophyll data --------------------------
 
-b <- dat_corr %>%
-  ggplot(mapping = aes(x = temp_c, y = chla_r, color = dilution_percent)) +
-  geom_point(size = 3) +
-  stat_smooth(method = "lm", se = FALSE) +
-  # ggpubr::stat_regline_equation(label.y.npc = 'bottom', show.legend = FALSE) +
-  scale_color_discrete(name = "Dilution Factor") +
-  # xlim(0, 40) +
-  theme_bw() +
-  theme(legend.position = "right",
-        axis.title = element_text(size = 12)) +
-  labs(y = chla_RFU_title,
-       x = 'Temperature ('~degree*C*')')
+plot.c <- dat_corr %>%
+            ggplot(mapping = aes(x = temp_c, y = chla_r)) +
+            stat_smooth(method = "lm", aes(color = dilution_percent), se = FALSE) +
+            geom_point(size = 3) +
+            scale_color_discrete(name = "Dilution Factor") +
+            theme_bw() +
+            theme(legend.position = "right",
+                  axis.title = element_text(size = 12)) +
+            labs(y = chla_RFU_title,
+                 x = 'Temperature ('~degree*C*')')
+plot.c
+
+plot.d <- dat_corr %>%
+            ggplot(mapping = aes(x = temp_c, y = chla_r_origin)) +
+            stat_smooth(method = "lm", aes(color = dilution_percent), se = FALSE) +
+            geom_point(size = 3) +
+            scale_color_discrete(name = "Dilution Factor") +
+            theme_bw() +
+            theme(legend.position = "right",
+                  axis.title = element_text(size = 12)) +
+            labs(y = chla_RFU_title,
+                 x = 'Temperature ('~degree*C*')')
+plot.d
 
 
 # 08 goodness of fit ------------------------------------------------------
